7055f48241ab2e1837ee97175e5a5a52555848ea
Corbin Barney <corbbarney@gmail.com>
2024-05-20 09:55:42 -0600

    altered comp file

--- compFiles.sh
+++ compFiles.sh
@@ -1,18 +1,13 @@
-FILE1='objects/newMembers2.txt'
-FILE2='objects/hasMinister.txt'
+FILE1='objects/perspectiveElders.txt'
+FILE2='objects/needsMinister.txt'
 
 for o1 in $(cat $FILE1)
 do
     for o2 in $(cat $FILE2)
     do
-        if [[ $(echo $o1 | cut -d '_' -f 2 | cut -d '-' -f 1) == $(echo $o2 | cut -d '_' -f 1 | cut -d '-' -f 1) ]]
+        if [[ $o1 == $o2 ]]
         then
-            if [[ $(echo $o1 | cut -d '_' -f 1 | cut -d '-' -f 1) == $(echo $o2 | cut -d '_' -f 2 | cut -d '-' -f 1) ]]
-            then
-                echo "$o1 : $o2"
-            else
-                echo "    $o1 : $o2"
-            fi
+            echo "$o1 : $o2"
         fi
     done
 done
